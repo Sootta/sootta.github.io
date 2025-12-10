c40bd9edfeb99a3e1c6797669a16a4b463538cc1
Sootta <soutaaibara@gmail.com>
2025-12-10 23:29:26 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='Top'>
+      <div className='container'>
         <h1>Sootta</h1>
         <p>高専3年｜組み込みとWebを勉強中</p>
         <a href="https://github.com/Sootta" target="_blank">→ GitHubを見る</a>
